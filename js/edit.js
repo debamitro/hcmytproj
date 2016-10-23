a1c52cf7e8eb22c09b121aca6bfae305d6143aa7
Debamitro Chakraborti <debamitro@gmail.com>
2016-10-23 03:13:48 +0000

    Initial work of search and edit

---
+++ js/edit.js
@@ -0,0 +1,97 @@
+// Google api needs to be included before this
+
+function onLoadFn() {
+    // make gapi.client calls
+    gapi.client.setApiKey(YOUTUBE_API_KEY);
+    gapi.client.load('youtube','v3').then(function() {
+	console.log('youtube api loaded');
+    });
+    
+}
+gapi.load("client", onLoadFn);
+
+function doSearch() {
+    $('#search-results').empty();
+    gapi.client.youtube.search.list({
+	'part' : 'snippet',
+	'q': $('#search-string').val()
+    }).then(function(resp) {
+	console.log(resp.result);
+	updateResults(resp.result.items);
+    }, function(reason) {
+	console.log('Error: ' + reason.result.error.message);
+    });	
+}
+
+function updateResults(result) {
+    result.forEach(function (oneresult) {
+	// create a result entry
+	var newDiv = $('<div/>').addClass('search-result-entry').addClass('container');
+
+	// add the title
+	$('<p>').text(oneresult.snippet.title).appendTo(newDiv);
+
+	var rowDiv = $('<div>').addClass('row');
+	var imgDiv = $('<div>').addClass('col-sm-4');
+	// add the image
+	$('<img>').attr({
+	    src : oneresult.snippet.thumbnails.default.url,
+	    width : oneresult.snippet.thumbnails.default.width,
+	    height : oneresult.snippet.thumbnails.default.height
+	}).appendTo(imgDiv);
+	
+	// add the link
+	$('<a>').attr({
+	    href : "https://www.youtube.com/watch?v=" + oneresult.id.videoId,
+	    target : '_blank'
+	}).text('watch').appendTo(imgDiv);
+	imgDiv.appendTo(rowDiv);
+	
+	var detailsDiv = $('<div>').addClass('col-sm-8');
+	$('<label>').text('Artist').appendTo(detailsDiv);
+	$('<input>').attr({
+	    id : 'video_' + oneresult.id.videoId + '_artist'
+	}).appendTo(detailsDiv);
+	$('<label>').text('Instrument').appendTo(detailsDiv);
+	$('<input>').attr({
+	    id : 'video_' + oneresult.id.videoId + '_instrument'
+	}).appendTo(detailsDiv);
+	$('<label>').text('Raga').appendTo(detailsDiv);
+	$('<input>').attr({
+	    id : 'video_' + oneresult.id.videoId + '_raga'
+	}).appendTo(detailsDiv);
+	$('<button>').text('save').click({
+	    id : oneresult.id.videoId,
+	    title : oneresult.snippet.title	    
+	}
+					 ,updateVideoData).appendTo(detailsDiv);
+	detailsDiv.appendTo(rowDiv);
+	rowDiv.appendTo(newDiv);
+	
+	// add the result entry
+	newDiv.appendTo('#search-results');
+    });
+}
+
+function updateVideoData (event) {
+    var vidArtist = '#video_' + event.data.id + '_artist';
+    var vidInstrument = '#video_' + event.data.id + '_instrument';
+    var vidRaga = '#video_' + event.data.id + '_raga';
+    console.log({
+	artist : $(vidArtist).val(),
+	instrument : $(vidInstrument).val(),
+	raga : $(vidRaga).val()
+    });	
+}
+
+$(function() {
+    // bind enter to search
+    $('#search-string').keypress(function (event) {
+	if (event.which == 13) {
+	    doSearch();
+	}
+    });
+
+    // bind click to search
+    $('#search-cmd').click(doSearch);
+});
---
+++ js/index.js
@@ -0,0 +1,135 @@
+// Google api needs to be included before this
+
+function onLoadFn() {
+    // make gapi.client calls
+    gapi.client.setApiKey(YOUTUBE_API_KEY);
+    gapi.client.load('youtube','v3').then(function() {
+        console.log('youtube api loaded');
+    });
+    
+}
+gapi.load("client", onLoadFn);
+
+$(function () {
+    loadSearchBar();
+    $('#search-cmd').click(updateResults);
+});
+
+function loadSearchBar () {
+    // populate raga list
+    var ragalist = [
+        'Bhairav',
+        'Bhairavi',
+        'Bihag',
+        'Yaman',
+        'Bhupali',
+        'Kafi',
+        'Kirwani',
+        'Vrindavani Sarang',
+        'Shuddh Sarang',
+        'Jaijaivanti',
+        'Jaunpuri',
+        'Pilu'
+    ];
+
+    ragalist.forEach(function (raga) {
+        $('<option>').attr({
+            value : raga
+        }).text(raga).appendTo('#search-raga');
+    });
+
+    // populate instrument list
+    var instrumentlist = [
+        'Vocal',
+        'Sarod',
+        'Sitar',
+        'Violin',
+        'Sarangi',
+        'Bansuri',
+        'Shehnai',
+        'Guitar'
+    ];
+
+    instrumentlist.forEach(function (instrument) {
+        $('<option>').attr({
+            value : instrument
+        }).text(instrument).appendTo('#search-instrument');
+    });
+
+    // populate decade list
+    var decadelist = [
+        '1910-1919',
+        '1920-1929',
+        '1930-1939',
+        '1940-1949',
+        '1950-1959',
+        '1960-1969',
+        '1970-1979',
+        '1980-1989',
+        '1990-1999'
+    ];
+
+    decadelist.forEach(function (decade) {
+        $('<option>').attr({
+            value : decade
+        }).text(decade).appendTo('#search-decade');
+    });
+
+}
+
+function updateResults () {
+    $('#search-results').empty();
+    var searchCriteria = [];
+    if ($('#search-raga').val() != '') {
+	searchCriteria.push({
+	    criterion : 'raga',
+	    value :    $('#search-raga').val()
+	});
+    }
+    if ($('#search-instrument').val() != '') {
+	searchCriteria.push({
+	    criterion : 'instrument',
+	    value :    $('#search-instrument').val()
+	});
+    }
+    if ($('#search-artist').val() != '') {
+	searchCriteria.push({
+	    criterion : 'artist',
+	    value :    $('#search-artist').val()
+	});
+    }
+
+    function record_matches (record, searchCriteria) {
+	var has_matched = true;
+
+	searchCriteria.forEach(function (elem) {
+	    if (elem.value != record[elem.criterion]) {
+		has_matched = false;
+	    }
+	});
+
+	return has_matched;
+    }
+
+    var matching_records = [];
+    the_database.forEach(function (record) {
+	if (record_matches(record, searchCriteria)) {
+	    matching_records.push(record.id);
+	}
+    });
+
+    gapi.client.youtube.videos.list({
+	part : 'snippet',
+	id : matching_records.join(',')
+    }).then(addResults);
+
+    function addResults (resp) {
+	resp.result.items.forEach(function(viditem) {
+	    var newDiv = $('<div>').text(viditem.snippet.title);
+	    $('<a>').attr({
+		href : 'https://www.youtube.com/watch?v=' + viditem.id
+	    }).text('watch').appendTo(newDiv);
+	    newDiv.appendTo('#search-results');
+	});
+    }
+}
