3b8086379a569531cbb4fe8267e5af2d821c1143
Debamitro Chakraborti <debamitro@gmail.com>
2017-09-17 01:37:06 +0000

    getting things in shape

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,6 @@
+/* eslint-env browser, jquery */
 // Google api needs to be included before this
+"use strict";
 
 // Load the Youtube API
 function onLoadFn() {
@@ -17,6 +19,9 @@ gapi.load("client", onLoadFn);
 $(function () {
     loadSearchBar();
     $('#search-cmd').click(updateResults);
+    $('#clear-cmd').click(function () {
+        $('#search-results').empty();
+    });
 });
 
 /**
@@ -36,7 +41,10 @@ function loadSearchBar () {
         'Shuddh Sarang',
         'Jaijaivanti',
         'Jaunpuri',
-        'Pilu'
+        'Pilu',
+        'Zila Kafi',
+        'Pahadi',
+        'Chhayanat'
     ];
 
     ragalist.forEach(function (raga) {
@@ -92,59 +100,70 @@ function updateResults () {
     $('#search-results').empty();
     var searchCriteria = [];
     if ($('#search-raga').val() != '') {
-	searchCriteria.push({
-	    criterion : 'raga',
-	    value :    $('#search-raga').val()
-	});
+        searchCriteria.push({
+            criterion : 'raga',
+            value :    $('#search-raga').val()
+        });
     }
     if ($('#search-instrument').val() != '') {
-	searchCriteria.push({
-	    criterion : 'instrument',
-	    value :    $('#search-instrument').val()
-	});
+        searchCriteria.push({
+            criterion : 'instrument',
+            value :    $('#search-instrument').val()
+        });
     }
     if ($('#search-artist').val() != '') {
-	searchCriteria.push({
-	    criterion : 'artist',
-	    value :    $('#search-artist').val()
-	});
+        searchCriteria.push({
+            criterion : 'artist',
+            value :    $('#search-artist').val()
+        });
     }
     if ($('#search-decade').val() != '') {
-	searchCriteria.push({
-	    criterion : 'decade',
-	    value :    $('#search-decade').val()
-	});
+        searchCriteria.push({
+            criterion : 'decade',
+            value :    $('#search-decade').val()
+        });
     }
 
     $.ajax({
-	url : '/videos/search',
-	data : { criteria : searchCriteria }
+        url : '/videos/search',
+        data : { criteria : searchCriteria }
     }).done (function (data) {
         var matching_records = [];
-	data.forEach(function (datum) {
-	    matching_records.push(datum.vid_ytid);
-	});
-	gapi.client.youtube.videos.list({
-	    part : 'snippet',
-	    id : matching_records.join(',')
-	}).then(addResults);
+        data.forEach(function (datum) {
+            matching_records.push(datum.vid_ytid);
+        });
+        gapi.client.youtube.videos.list({
+            part : 'snippet',
+            id : matching_records.join(',')
+        }).then(addResults);
     });
     
     function addResults (resp) {
-	resp.result.items.forEach(function(viditem) {
-	    var newDiv = $('<div>').addClass('result-elem');
-	    $('<p>').text(viditem.snippet.title).appendTo(newDiv);
-	    $('<img>').attr({
-		src : viditem.snippet.thumbnails.default.url,
-		width : viditem.snippet.thumbnails.default.width,
-		height : viditem.snippet.thumbnails.default.height
-	    }).appendTo(newDiv);
-	    var newPar = $('<p>');
-	    $('<a>').attr({
-		href : 'https://www.youtube.com/watch?v=' + viditem.id
-	    }).text('watch').appendTo(newPar);
-	    newPar.appendTo(newDiv);
-	    newDiv.appendTo('#search-results');
-	});
+        resp.result.items.forEach(function(viditem) {
+            var newDiv = $('<div>').addClass('result-elem');
+            $('<div>').addClass('result-elem-title').text(viditem.snippet.title).appendTo(newDiv);
+            var imgDiv = $('<div>').addClass('result-elem-img');
+            $('<img>').attr({
+                src : viditem.snippet.thumbnails.default.url,
+                width : viditem.snippet.thumbnails.default.width,
+                height : viditem.snippet.thumbnails.default.height
+            }).appendTo(imgDiv);
+            imgDiv.appendTo(newDiv);
+            $('<div>').addClass('result-elem-description').text(viditem.snippet.description.substring(0,200) + " ...").appendTo(newDiv);
+            $('<div>').addClass('spacer').appendTo(newDiv);
+            var watchDiv = $('<div>');
+            var newLink = $('<a>').addClass('result-elem-ytlink').attr({
+                href : 'https://www.youtube.com/watch?v=' + viditem.id,
+                target : '_blank'
+            }).text('Watch on ');
+            $('<img>').attr({
+                src: 'images/youtube_social_icon_red.png',
+                width: '32',
+                height: '22'
+            }).appendTo(newLink);
+            newLink.appendTo(watchDiv);
+            watchDiv.appendTo(newDiv);
+            newDiv.appendTo('#search-results');
+        });
     }
 }
